f32fe8ea9e057f7f53a5ebf0db7d3b511a3c8da0
Marcos Schlesser <schlesserr@gmail.com>
2021-08-22 15:49:11 -0300

    improving base installation of arch

--- base.sh
+++ base.sh
@@ -1,38 +1,30 @@
-ln -sf /usr/share/zoneinfo/Americas/Belem /etc/localtime
+ln -sf /usr/share/zoneinfo/America/Belem /etc/localtime
 hwclock --systohc
 sed -i '177s/.//' /etc/locale.gen
 locale-gen
 echo "LANG=pt_BR.UTF-8" >> /etc/locale.conf
 echo "KEYMAP=br-abnt2" >> /etc/vconsole.conf
-echo "arch" >> /etc/hostname
+echo "archlinux" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
-echo root:password | chpasswd
+echo "127.0.1.1 archlinux.localdomain archlinux" >> /etc/hosts
 
-# You can add xorg to the installation packages, I usually add it at the DE or WM install script
-# You can remove the tlp package if you are installing on a desktop or vm
+pacman -S grub reflector base-devel linux-headers  bluez bluez-utils pulseaudio openssh rsync reflector acpi acpi_call acpid tlp os-prober ntfs-3g terminus-font iwctl
 
-pacman -S grub efibootmgr reflector base-devel linux-headers  bluez bluez-utils cups hplip alsa-utils pulseaudio bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
-
-# pacman -S --noconfirm xf86-video-amdgpu
-# pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+pacman -S  nvidia nvidia-utils 
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
-
 systemctl enable bluetooth
-systemctl enable cups.service
 systemctl enable reflector.timer
 systemctl enable fstrim.timer
 
 useradd -m schlesser
-echo ermanno:password | chpasswd
-usermod -aG libvirt ermanno
+echo schlesser:123 | chpasswd
+usermod -aG schlesser
 
 echo "schlesser ALL=(ALL) ALL" >> /etc/sudoers.d/schlesser
 
 
 printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
-
